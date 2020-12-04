d248ab6d33cc6d21c22b7031ee0a267f44a37e51
Santiago <santi16648@gmail.com>
2020-12-04 17:36:03 -0400

    fix: protocol http or https implemented

--- public/scripts/js/helpers/config.js
+++ public/scripts/js/helpers/config.js
@@ -1,4 +1,4 @@
 //window.location.protocol
-export const PROTOCOL =  "https://";
+export const PROTOCOL =  window.location.protocol + "//";
 export const BASE_URL = PROTOCOL + window.location.host;
 export const VACCINATIONS_URL = BASE_URL + "/vacunas";
