0274610826b134fcaf8cfd5ec839595d1b4d8148
znareak <libardojesusrengifo129@gmail.com>
2020-12-16 18:22:14 -0430

    feat: view X person

--- resources/views/persons/person-show.blade.php
+++ resources/views/persons/person-show.blade.php
@@ -2,23 +2,71 @@
 @section("title", "Sistema vacunativo » Crear persona")
 @section("body")
     @include("components.navbar.navbar-auth")
+    <section class="container mt-5">
+        <div class="d-flex align-items-center">
+            <a href="/personas" class="btn btn-primary mr-3"><i class="fa fa-arrow-left"></i></a>
+            <h2 class="title">Perfil de {{$person->first_name}}</h2>
+        </div>
+    </section>
 
     <section class="container mt-5">
         <div class="row">
-            <img class="rounded-circle" src="{{Storage::url($person->image_url)}}" alt="Imagen de perfil de {{$person->fullName}}" width="100px" height="100px">
-            <div class="ml-4">
-                <h5>Nombres: <strong>{{ $person->fullName }}</strong></h5>
-                <h5>Cedula: {{ $person->dni }}</h5>
-                <h5>Edad: {{ $person->age }}</h5>
-                <h5>Casa: {{ $person->house->number }}</h5>
-                <h5>Calle: {{ $person->house->street->name }}</h5>
-                <h5>Numero telefonico: {{ $person->phone_number }}</h5>
+            <div class="col-lg-3">
+                <img class="shadow-sm d-block" 
+                 src="{{Storage::url($person->image_url)}}" 
+                 alt="Imagen de perfil de {{$person->fullName}}" 
+                 title="Imagen de perfil de {{$person->fullName}}" 
+                 width="100%" height="220px" style="object-fit: cover;"/>
+
+                <div class="btn-group mt-2 w-100" role="group">
+                    <a class="btn btn-outline-primary" href="/personas/{{$person->id}}/editar">
+                        <i class="fa fa-pencil ml-1"></i>
+                    </a>
+
+                    <a class="btn btn-outline-info" href="/personas/{{$person->id}}/vacunas">
+                        <i class="fa fa-list-ul ml-1"></i>
+                    </a>
+                </div>
+            </div>
+
+            <div class="col-4 px-0">
+                <div class="ml-4">
+                    <h6>
+                        <span class="font-weight-bold mr-2">Nombres:</span>
+                        <span>{{ $person->fullName }}</span>
+                    </h6>
+                    <h6>
+                        <span class="font-weight-bold mr-2">Edad:</span>
+                        <span>{{ $person->age }}</span>
+                    </h6>
+                    <h6>
+                        <span class="font-weight-bold mr-2">Casa:</span>
+                        <span>Nro. {{ $person->house->number }}</span>
+                    </h6>
+                    <h6>
+                        <span class="font-weight-bold mr-2">Calle:</span>
+                        <span>{{ $person->house->street->name }}</span>
+                    </h6>
+                    <h6>
+                        <span class="font-weight-bold mr-2">Teléfono:</span>
+                        <span class="text-primary">{{ $person->phone_number }}</span>
+                    </h6>
+
+                    <hr class="mt-4"/>
+
+                    <h6>
+                        <span class="font-weight-bold mr-2">Fecha de creación:</span>
+                        {{ date("d-m-Y", strtotime($person->created_at)) }}
+                    </h6>
+
+                    <h6>
+                        <span class="font-weight-bold mr-2">Fecha de modificación:</span>
+                        {{ date("d-m-Y", strtotime($person->updated_at)) }}
+                    </h6>
+                </div>
             </div>
         </div>
-        <div class="mt-5">
-            <h5><strong><i class="fa fa-calendar"></i>Fecha de creacion:</strong>{{$person->created_at}}</h5>
-            <h5><strong><i class="fa fa-calendar"></i>Fecha de modificacion:</strong>{{$person->updated_at}}</h5>    
-        </div>
+
         @if($person->father || $person->mother))
             <div>
                 <h3>Padres:</h3>
@@ -38,9 +86,7 @@
                 @endforeach
             <div> 
         @endif
-        
-
-
+    
     </section>
 @include("components.footer.footer")
 @endsection
