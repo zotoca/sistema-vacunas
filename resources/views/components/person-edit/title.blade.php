5ab8bba54a50949e82faffb2ff798d61749a5daa
znareak <libardojesusrengifo129@gmail.com>
2020-12-21 15:17:38 +0100

    feat: module components

---
+++ resources/views/components/person-edit/title.blade.php
@@ -0,0 +1,6 @@
+<section class="container mt-5">
+   <div class="d-flex align-items-center">
+      <a href="/personas" class="btn btn-primary mr-3"><i class="fa fa-arrow-left"></i></a>
+      <h2 class="title title-big">Editar persona</h2>
+   </div>
+</section>
