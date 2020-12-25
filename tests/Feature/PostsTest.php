ead303e6e880ee6e7f5f67abccd633cd21544dce
Santiago <santi16648@gmail.com>
2020-12-25 19:16:56 -0400

    test: edit post tests added

--- tests/Feature/PostsTest.php
+++ tests/Feature/PostsTest.php
@@ -88,14 +88,70 @@ class PostsTest extends TestCase
 
     public function test_an_administrator_can_upload_content_images(){
         $this->signIn();
-
+        
         $attributes = [
-            "image" => UploadedFile::fake()->image("post.jpg")
+            "file" => UploadedFile::fake()->image("post.jpg")
         ];
 
         $this->post("/foro/subir-imagen", $attributes)
             ->assertStatus(200)
             ->assertJsonStructure(["location"]);
+    }
+
+    public function test_an_administrator_can_see_edit_post(){
+        $this->signIn();
+        $this->withoutExceptionHandling();
+
+        $post = Post::factory()->create();
+
+        $this->get($post->path()."/editar")
+            ->assertStatus(200)
+            ->assertSee($post->title)
+            ->assertSee($post->content);
+    }
+
+    public function test_an_administrator_can_edit_a_post(){
+        $this->signIn();
+        
+        
+        $post = Post::factory()->create();
+
+        $attributes = ["title" => "test post title"];
+
+        $this->put($post->path(), $attributes)
+            ->assertRedirect($post->path());
+
+
+        $this->get("/foro")
+            ->assertStatus(200)
+            ->assertSee($attributes["title"]);
+
+        $this->assertDatabaseHas("posts", ["id" => $post->id, "title" => $attributes["title"]]);
+    }
+
+    
+    public function test_an_administrator_can_edit_a_post_with_image(){
+        $this->signIn();
+
+        $old_image_url = Storage::disk("public")->putFile("/post-images",UploadedFile::fake()->image("post.jpg"));
+
+        $post = Post::factory()->create(["image_url" => $old_image_url]);
+
+        $attributes = ["image" => UploadedFile::fake()->image("post.jpg")];
+
+        $this->put($post->path(), $attributes)
+            ->assertRedirect($post->path());
+
+
+        $this->get("/foro")
+            ->assertStatus(200)
+            ->assertSee($post->title);
+
+        Storage::disk("public")->assertMissing($old_image_url);
+        
+        $post->refresh();
+
+        Storage::disk("public")->assertExists($post->image_url);
     }
 
     public function test_an_administrator_can_delete_a_post(){
