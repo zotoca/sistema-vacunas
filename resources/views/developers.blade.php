71fa8446129222e8b647c9b1d0fd809995e094ec
Santiago <santi16648@gmail.com>
2021-01-23 11:08:27 -0400

    style: add necromancer photo and bio

--- resources/views/developers.blade.php
+++ resources/views/developers.blade.php
@@ -23,14 +23,25 @@
             </div>
         </div>
         <br />
-        <div class="row my-5">
+        <div class="row flex-column-reverse flex-md-row my-5">
             <div class="col-12 col-md-6 mt-5 mt-md-0" data-aos="fade-up" data-aos-delay="300">
+            
                 <h4 class="title text-uppercase mb-4">Santiago Padron</h4>
                 <p  class="lead mt-2">
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                    Debitis perspiciatis quidem distinctio impedit omnis magni ea suscipit itaque non tenetur, 
-                    fugit voluptates nesciunt error magnam voluptatum in aut. Aut, maxime.
-                    Lorem ipsum dolor
+                Desarrollador web
+                PHP + Javascript,
+                amante de las tecnologias y del aprendizaje.<br/>
+                La educacion formal la mayoria de las veces
+                nos enseña que aprender, pero no como aprenderlo.
+
+                <blockquote class="blockquote">
+                    <p class="mb-0">
+                        Dame 6 horas para cortar un árbol y pasaré 4 afilando el hacha
+                    </p>
+                    <footer class="blockquote-footer">
+                        Abraham Lincoln
+                    </footer>
+                <blockquote>
                 </p>
                 <p class="lead">
                     <a href="https://github.com/N3CROM4NC3R" class="text-reset">
@@ -39,7 +50,7 @@
                 </p>
             </div>
             <div class="col-12 col-md-6" data-aos="fade-up">
-                <img class="img-fluid w-100" src="{{asset("/images/persons.png")}}">
+                <img class="img-fluid w-100" src="{{asset("/images/dev/necromancer.jpg")}}">
             </div>
         </div>
     </div>
