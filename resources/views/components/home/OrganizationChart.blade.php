416347c08fdf12b61eafdab139c0fb2a3d97a71b
Santiago <santi16648@gmail.com>
2021-02-21 18:51:15 -0400

    style: organizacion chart abstract added

--- resources/views/components/home/OrganizationChart.blade.php
+++ resources/views/components/home/OrganizationChart.blade.php
@@ -1,12 +1,8 @@
 <section class="section" id="organigrama" data-aos="fade-up">
     <h2 class="title title-big title-underline">Organigrama</h2>
     <p class="lead mt-5">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab qui 
-        cupiditate corrupti officiis quia sit aspernatur iste magni culpa non?
-        Labore sapiente, corporis saepe iure, alias totam non provident ipsum 
-        perspiciatis excepturi minus, aliquid atque quisquam voluptatibus. 
-        Animi eaque possimus voluptatibus ipsam.
+        Bases para la organizacion, planificación y ejecución de todo lo que tiene que ver en materia y planteamientos para ejercer el plan comunal de cada comunidad, que parte desde el año 2006 donde se crearon varias leyes para dicho objetivo, entre ellas, Ley Organica de los Consejos Comunales, Ley Organica del Poder Popular y la Ley Organica de las Comunas.
     </p>
     <img src="{{asset("images/organigrama.png")}}" 
-    alt="Organigrama de la urbaniación" class="img-fluid mx-auto">
+    alt="Organigrama de la urbanización" class="img-fluid mx-auto">
 </section>
