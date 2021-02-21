7a532c835da026e3fec5649e4d0072d1fbd6c85c
znareak <libardojesusrengifo129@gmail.com>
2021-02-21 17:39:54 +0100

    feat: Dashboard modules texts changed!

--- resources/views/components/dashboard/actions.blade.php
+++ resources/views/components/dashboard/actions.blade.php
@@ -6,7 +6,11 @@
                 <img class="card-img-top img-fluid" src="{{asset("images/vacunas.png")}}">
                 <div class="card-body">
                     <h3 class="card-title title">Vacunas</h3>
-                    <p class="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi neque reiciendis sequi atque soluta, laudantium nostrum laborum consequatur distinctio p</p>
+                    <p class="card-text">
+                        Módulo referente a las vacunas del sector morichal, las cuales
+                        serán asignadas a las personas que habitan en la comunidad. 
+                        Precaución en la gestión de este módulo.
+                    </p>
                     <a href="/vacunas" class="btn btn-block btn-primary stretched-link">Seleccionar</a>
                 </div>
             </div>
@@ -17,7 +21,10 @@
                 <img class="card-img-top img-fluid" src="{{asset("images/persons.png")}}">
                 <div class="card-body">
                     <h3 class="card-title title">Personas</h3>
-                    <p class="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi neque reiciendis sequi atque soluta, laudantium nostrum laborum consequatur distinctio p</p>
+                    <p class="card-text">
+                        Módulo referente a las personas que habitan el sector morichal.
+                        Este módulo hace uso del módulo de vacunas para administrar las vacunas de las personas.
+                    </p>
                     <a href="/personas" class="btn btn-block btn-primary stretched-link">Seleccionar</a>
                 </div>
             </div>
@@ -28,7 +35,10 @@
                 <img class="card-img-top img-fluid" src="{{asset("images/admins.png")}}">
                 <div class="card-body">
                     <h3 class="card-title title">Administradores</h3>
-                    <p class="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi neque reiciendis sequi atque soluta, laudantium nostrum laborum consequatur distinctio p</p>
+                    <p class="card-text">
+                        Módulo refente a las personas con roles de administradores del sistema, estás
+                        pueden realizar acciones de alto privilegio en los módulos anteriores.
+                    </p>
                     <a href="/administradores" class="btn btn-block btn-primary stretched-link">Seleccionar</a>
                 </div>
             </div>
@@ -39,7 +49,9 @@
                 <img class="card-img-top img-fluid" src="{{asset("images/foro.jpg")}}">
                 <div class="card-body">
                     <h3 class="card-title title">Foro</h3>
-                    <p class="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi neque reiciendis sequi atque soluta, laudantium nostrum laborum consequatur distinctio p</p>
+                    <p class="card-text">
+                        Módulo referente a dudas, problemas o debates relacionado al sistema/vacunas.
+                    </p>
                     <a href="/foro" class="btn btn-block btn-primary stretched-link">Seleccionar</a>
                 </div>
             </div>
@@ -50,7 +62,9 @@
                 <img class="card-img-top img-fluid" src="{{asset("images/news.jpg")}}">
                 <div class="card-body">
                     <h3 class="card-title title">Noticias</h3>
-                    <p class="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi neque reiciendis sequi atque soluta, laudantium nostrum laborum consequatur distinctio p</p>
+                    <p class="card-text">
+                        Módulo referente a la gestión de noticias o actividades relacionadas a las vacunas.
+                    </p>
                     <a href="/noticias" class="btn btn-block btn-primary stretched-link">Seleccionar</a>
                 </div>
             </div>
