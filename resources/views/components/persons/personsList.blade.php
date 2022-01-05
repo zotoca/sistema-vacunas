03815b19978427411c3e760d58c823899cee62c5
Libardo <libardojesusrengifo129@gmail.com>
2022-01-05 13:08:47 -0400

    feat: show delete buttons if the current admin can do that action

--- resources/views/components/persons/personsList.blade.php
+++ resources/views/components/persons/personsList.blade.php
@@ -1,47 +1,48 @@
 <section class="container mt-5" data-aos="fade-up">
     <div class="row">
         @forelse($persons as $person)
-        <div class="col-sm-12 col-md-6 col-lg-4 px-2 py-2">
-            <div class="card">
-                <img class="card-img-top person-image" 
-                    src="{{Storage::url($person->image_url)}}" 
-                    alt="{{$person->first_name}}"
-                    title="{{$person->first_name}}"
-                >
-                <div class="card-body">
-                    <h5 class="card-title title pl-1">{{$person->first_name}} {{$person->last_name}}</h5>
-                    <div class="row w-100 m-0">
-                        <div class="col-sm-12 col-lg-6 p-1">
-                            <a class="btn btn-primary btn-block" href="/personas/{{$person->id}}/editar">
-                                Editar
-                                <i class="fa fa-edit ml-1"></i>
-                            </a>
+            <div class="col-sm-12 col-md-6 col-lg-4 px-2 py-2">
+                <div class="card">
+                    <img class="card-img-top person-image" src="{{ Storage::url($person->image_url) }}"
+                        alt="{{ $person->first_name }}" title="{{ $person->first_name }}">
+                    <div class="card-body">
+                        <h5 class="card-title title pl-1">{{ $person->first_name }} {{ $person->last_name }}</h5>
+                        <div class="row w-100 m-0">
+                            <div class="col-sm-12 col-lg-6 p-1">
+                                <a class="btn btn-primary btn-block" href="/personas/{{ $person->id }}/editar">
+                                    Editar
+                                    <i class="fa fa-edit ml-1"></i>
+                                </a>
+                            </div>
+                            
+                            <!-- If the admin CAN delete the person data -->
+                            @can('remove person')
+                                <div class="col-sm-12 col-lg-6 p-1">
+                                    <a class="btn btn-danger btn-block" data-id="{{ $person->id }}" data-action="delete">
+                                        Eliminar
+                                        <i class="fa fa-trash-alt ml-1"></i>
+                                    </a>
+                                </div>
+                            @endcan
                         </div>
-                        <div class="col-sm-12 col-lg-6 p-1">
-                            <a class="btn btn-danger btn-block" data-id="{{$person->id}}" data-action="delete">
-                                Eliminar
-                                <i class="fa fa-trash-alt ml-1"></i>
-                            </a>
-                        </div>
-                    </div>
 
-                    <div class="row w-100 m-0">
-                        <div class="col-sm-12 col-lg-6 p-1">
-                            <a class="btn btn-info btn-block" href="/personas/{{$person->id}}/vacunas-personas">
-                                Vacunas
-                                <i class="fa fa-syringe ml-1"></i>
-                            </a>
-                        </div>
-                        <div class="col-sm-12 col-lg-6 p-1">
-                            <a class="btn btn-info btn-block" href="/personas/{{$person->id}}">
-                                Ver perfil
-                                <i class="fa fa-eye ml-1"></i>
-                            </a>
+                        <div class="row w-100 m-0">
+                            <div class="col-sm-12 col-lg-6 p-1">
+                                <a class="btn btn-info btn-block" href="/personas/{{ $person->id }}/vacunas-personas">
+                                    Vacunas
+                                    <i class="fa fa-syringe ml-1"></i>
+                                </a>
+                            </div>
+                            <div class="col-sm-12 col-lg-6 p-1">
+                                <a class="btn btn-info btn-block" href="/personas/{{ $person->id }}">
+                                    Ver perfil
+                                    <i class="fa fa-eye ml-1"></i>
+                                </a>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
         @empty
             @include("components.notResults.notResults")
         @endforelse
--- resources/views/components/vaccinations/vaccinationList.blade.php
+++ resources/views/components/vaccinations/vaccinationList.blade.php
@@ -1,28 +1,36 @@
 <section class="container mt-5" data-aos="fade-up">
     <div class="row">
         @forelse($vaccinations as $vaccination)
-        <div class="col-sm-12 col-md-6 col-lg-4 px-2 py-2" >
-            <div class="card">
-                <img class="card-img-top" src="{{asset("/images/vacunas.png")}}" alt="{{$vaccination->name}}" title="{{$vaccination->name}}">
-                <div class="card-body">
-                    <h5 class="card-title title pl-1">{{$vaccination->name}}</h5>
-                    <div class="row w-100 m-0">
-                        <div class="col-sm-12 col-lg-6 p-1">
-                            <button class="btn btn-primary btn-block" data-id="{{$vaccination->id}}" data-action="edit">
-                                Editar
-                                <i class="fa fa-edit ml-1"></i>
-                            </button>
-                        </div>
-                        <div class="col-sm-12 col-lg-6 p-1">
-                            <button class="btn btn-danger btn-block" data-id="{{$vaccination->id}}" data-action="delete">
-                                Eliminar
-                                <i class="fa fa-trash-alt ml-1"></i>
-                            </button>
+            <div class="col-sm-12 col-md-6 col-lg-4 px-2 py-2">
+                <div class="card">
+                    <img class="card-img-top" src="{{ asset('/images/vacunas.png') }}"
+                        alt="{{ $vaccination->name }}" title="{{ $vaccination->name }}">
+                    <div class="card-body">
+                        <h5 class="card-title title pl-1">{{ $vaccination->name }}</h5>
+                        <div class="row w-100 m-0">
+                            <div class="col-sm-12 col-lg-6 p-1">
+                                <button class="btn btn-primary btn-block" data-id="{{ $vaccination->id }}"
+                                    data-action="edit">
+                                    Editar
+                                    <i class="fa fa-edit ml-1"></i>
+                                </button>
+                            </div>
+
+                            <!-- If the admin CAN delete the vaccine data -->
+                            @can('remove vaccine')
+                                <div class="col-sm-12 col-lg-6 p-1">
+                                    <button class="btn btn-danger btn-block" data-id="{{ $vaccination->id }}"
+                                        data-action="delete">
+                                        Eliminar
+                                        <i class="fa fa-trash-alt ml-1"></i>
+                                    </button>
+                                </div>
+                            @endcan
+
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
         @empty
             @include("components.notResults.notResults")
         @endforelse
