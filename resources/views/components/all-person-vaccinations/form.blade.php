96a54a2cd528a6d1f5376182abfa0e829fc9064b
Libardo <libardojesusrengifo129@gmail.com>
2022-01-07 13:01:23 -0400

    dev: CSS for the all-person-vaccinations view

--- resources/views/components/all-person-vaccinations/form.blade.php
+++ resources/views/components/all-person-vaccinations/form.blade.php
@@ -1,60 +1,64 @@
 <section class="container mt-5" data-aos="fade-up">
     <div class="row">
-        <div class="col-md-12 col-lg-12 text-center">
-            <form class="form-inline text-center mx-auto">
-                <div class="input-group w   -100">
+        <div class="col-md-12 col-lg-12">
+            <form class="form-inline mx-auto">
 
-                    <select name="vaccination-id" class="form-control">
-                        <option value="" selected disabled>Selecciona tu vacuna</option>
-                        @foreach ($vaccinations as $vaccination)
-                            <option value="{{ $vaccination->id }}">{{ $vaccination->name }}</option>
-                        @endforeach
-                    </select>
-                    
-                    <input type="text" name="lot-number" placeholder="Numero de lote" class="form-control" />
-                    <div class="input-group w-100">
-                        <h2 class="font-weight-bold mb-2 lead mt-2">
-                            Buscar desde una dosis y dia
-                        </h2>
-                        <div class="input-group w-100 mt-2">
-                            
-                            <label for="days">Buscar desde el dia: </label>
-                            <input type="number" name="days" placeholder="Días" class="form-control" />
-                        </div>
-                        <div class="input-group w-100 mt-2">
-                            <label for="after-dose">Buscar desde la dosis: </label>
-                            <select class="form-control" name="after-dose" id="person-vaccination-dose"  placeholder="Dosis">
-                                <option value=1>1</option>
-                                <option value=2>2</option>
-                                <option value=3>3</option>
-                                <option value=4>4</option>
-                                <option value=5>5</option>
-                                <option value=6>6</option>
-                                <option value=7>7</option>
-                                <option value=8>8</option>
-                                <option value=9>9</option>
-                            </select>
-                            
-                        </div>
-                    </div>
-
-                    <div class="input-group w-100 mt-2">
-                        <input type="date" name="vaccination-date" placeholder="Fecha de vacunacion"
-                            class="form-control" />
-                    </div>
-                    <div class="input-group w-100 mt-2">
-                        <select name="is-vaccinated" class="form-control">
-                            <option value="" selected>Selecciona el estado de la vacuna</option>
-                            <option value="1">Vacunado</option>
-                            <option value="0">No vacunado</option>
+                <div class="row m-0 p-0 w-100">
+                    <div class="col-sm-12 col-md-6 col-lg-6 pl-0 pr-1">
+                        <select name="vaccination-id" class="form-control d-block w-100">
+                            <option value="" selected disabled>Selecciona tu vacuna</option>
+                            @foreach ($vaccinations as $vaccination)
+                                <option value="{{ $vaccination->id }}">{{ $vaccination->name }}</option>
+                            @endforeach
                         </select>
+                    </div>
+                    <div class="col-sm-12 col-md-6 col-lg-6 pr-0">
+                        <input type="text" name="lot-number" placeholder="Numero de lote"
+                            class="form-control d-block w-100" />
                     </div>
                 </div>
 
 
 
-                <div class="row w-100 mt-2">
-                    <div class="col-12">
+
+                <div class="row m-0 p-0 w-100 mt-1">
+                    <div class="col-12 p-0">
+                        <h2 class="font-weight-bold mb-2 lead mt-2">
+                            Buscar desde una dosis y día
+                        </h2>
+                    </div>
+                    <div class="col-sm-12 col-md-4 col-lg-4 p-0">
+                        <label for="days" class="d-block w-100">Día </label>
+                        <input type="number" name="days" placeholder="Días" class="form-control d-block w-100" />
+                    </div>
+
+                    <div class="col-sm-12 col-md-4 col-lg-4 p-0 px-1">
+                        <label for="after-dose" class="d-block w-100">Dosís </label>
+                        <select class="form-control d-block w-100" name="after-dose" id="person-vaccination-dose"
+                            placeholder="Dosis">
+                            @for ($i = 1; $i <= 10; $i++)
+                                <option value="{{ $i }}">{{ $i }}</option>
+                            @endfor
+                        </select>
+                    </div>
+
+                    <div class="col-sm-12 col-md-4 col-lg-4 p-0">
+                        <label for="date" class="d-block w-100">Fecha </label>
+                        <input type="date" name="vaccination-date" placeholder="Fecha de vacunacion" id="date"
+                            class="form-control d-block w-100" />
+                    </div>
+                </div>
+
+                <div class="row w-100 p-0 m-0 mt-2">
+                    <div class="col-sm-12 col-md-6 col-lg-6 p-0 pr-1">
+                        <select name="is-vaccinated" class="form-control d-block w-100">
+                            <option value="" selected>Selecciona el estado de la vacuna</option>
+                            <option value="1">Vacunado</option>
+                            <option value="0">No vacunado</option>
+                        </select>
+                    </div>
+
+                    <div class="col-sm-12 col-md-6 col-lg-6 p-0">
                         <button class="btn btn-primary btn-block">
                             Buscar <i class="fa fa-search ml-1"></i>
                         </button>
